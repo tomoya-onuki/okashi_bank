fdb39e71c6b3ffc37eabeffec4c2a16e42b65c19
tomoya-onuki <tomoya.onuki28@gmail.com>
2022-02-24 20:42:26 +0900

    複数のモデルを出す

--- app/js/main.js
+++ app/js/main.js
@@ -1,10 +1,13 @@
 $(function () {
-
+    const numOfModel = [
+        10, 5, 6
+    ];
     const model = [
         'koala.glb', 'gahana.glb', 'pie.glb'
     ];
 
     let model_idx = 0;
+    initModel();
 
     // $('.ar_model').each( function(idx) {
     //     // if (idx === 0) {
@@ -22,8 +25,8 @@ $(function () {
         } else if (model_idx >= model.length - 1) {
             model_idx = 0;
         }
-        $('#ar_model').attr('gltf-model', './obj/' + model[model_idx]);
-        $('#v_model').attr('src', './obj/' + model[model_idx]);
+        updateModel();
+        // $('#v_model').attr('src', './obj/' + model[model_idx]);
     });
     $('#right_btn').on('click', function () {
         console.log(model_idx);
@@ -32,7 +35,30 @@ $(function () {
         } else if (model_idx <= 0) {
             model_idx = model.length - 1;
         }
-        $('#ar_model').attr('gltf-model', './obj/' + model[model_idx]);
-        $('#v_model').attr('src', './obj/' + model[model_idx]);
+        updateModel();
     });
+
+
+    $('#ar_model').on('click', function () {
+        console.log("grab!");
+    });
+
+
+    function initModel() {
+        for (let i = 0; i < numOfModel[model_idx]; i++) {
+            let x = Math.random(-10, 10);
+            let y = Math.random(-10, 10);
+            let z = Math.random(-10, 10);
+            let r = Math.round(Math.random(1)) * 90;
+            let elem = '<a-entity class="ar_model" position="' + x + ' 0 ' + z + '" rotation="0 '+r+' 0" scale="2 2 2" gltf-model="./obj/koala.glb"></a-entity>';
+            $('#a-marker').append(elem);
+        }
+    }
+
+    function updateModel() {
+        // $('#ar_model').attr('gltf-model', './obj/' + model[model_idx]);
+        $('.ar_model').each(function () {
+            $(this).attr('gltf-model', './obj/' + model[model_idx]);
+        });
+    }
 });
