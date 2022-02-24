4d6f88dab34c2c8b50b0f1eed4787157e329abc4
tomoya-onuki <tomoya.onuki28@gmail.com>
2022-02-24 14:38:34 +0900

    AR

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>お菓子銀行</title>
+</head>
+<body>
+    <h1>お菓子銀行</h1>
+    <div>
+        <a href="./app/">開発中アプリ</a>
+    </div>
+
+    <div>
+        <a href="./moc/">モック</a>
+    </div>
+</body>
+</html>
