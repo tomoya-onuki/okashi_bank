723dacba2ec24cac1a6971be666ba61e82feaa6c
tomoya-onuki <tomoya.onuki28@gmail.com>
2022-02-24 17:30:23 +0900

    ARモデル、順序で表示

---
+++ app/js/main.js
@@ -0,0 +1,35 @@
+$(function () {
+
+    const model = [
+        'koala.glb', 'gahana.glb', 'pie.glb'
+    ];
+
+    let model_idx = 0;
+
+    // $('.ar_model').each( function(idx) {
+    //     // if (idx === 0) {
+    //     //     $(this).show();
+    //     //     console.log(idx);
+    //     // }
+    // });
+    $('#ar_model').attr('gltf-model', './obj/' + model[model_idx]);
+
+    $('#left_btn').on('click', function () {
+        console.log(model_idx);
+        if (model_idx < model.length - 1) {
+            model_idx++;
+        } else if (model_idx >= model.length - 1) {
+            model_idx = 0;
+        }
+        $('#ar_model').attr('gltf-model', './obj/' + model[model_idx]);
+    });
+    $('#right_btn').on('click', function () {
+        console.log(model_idx);
+        if (model_idx > 0) {
+            model_idx--;
+        } else if (model_idx <= 0) {
+            model_idx = model.length - 1;
+        }
+        $('#ar_model').attr('gltf-model', './obj/' + model[model_idx]);
+    });
+});
